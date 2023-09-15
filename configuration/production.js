7652832d9cdd8287ddd663f682d26abc7011b4c4
yordandanchev <yordangdanchev@gmail.com>
2023-09-15 13:39:42 +0300

    - typescript support

--- configuration/production.js
+++ configuration/production.js
@@ -9,7 +9,30 @@ const productionConfig = {
     new CleanWebpackPlugin({
       root: path.resolve(paths.root, './dist')
     })
-  ]
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 20000,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    }
+  }
 };
 
 export const webpackProdConfig = merge(common, productionConfig);
